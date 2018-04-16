c40453ed403c36f9801c014686bbc2cb5dff784b
Nestor Valentinis <nesti07@gmail.com>
2018-04-16 19:52:18 -0300

    Nestor - Add react-router lib, configure Routers and Router maps

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+// https://reduxblog.herokuapp.com/
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserhistory } from 'react-router';
 
-import App from './components/app';
+import routes from './routes';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history= {browserhistory} routes={routes}/>
   </Provider>
   , document.querySelector('.container'));
