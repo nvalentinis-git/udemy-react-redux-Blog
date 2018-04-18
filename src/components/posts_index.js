b9fd59c9cada640ddf6c1b4c99fde6c66b44fe29
Nestor Valentinis <nesti07@gmail.com>
2018-04-18 20:10:51 -0300

    Nestor - Add Fetch Post Acction Cretor on PostIndex Container

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
-export default () => {
-  return <div>List of post</div>
+class PostIndex extends Component {
+
+  render() {
+    return (
+      <div>List of blog</div>
+    );
+  }
+
+  componentWillMount() {
+    this.props.fetchPost();
+  }
 }
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators( {fetchPost}, dispatch );
+// }
+// Insted of using this function to map the Action Creator into the
+//  component, you can jsut pas an object with the name of
+//  the Action Creator { fetchPost: fetchPost }
+
+export default connect(null, { fetchPost: fetchPost }) (PostIndex);
