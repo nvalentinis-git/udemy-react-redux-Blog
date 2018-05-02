08b63f86f8d4840464909082749940d25e8c823b
Nestor Valentinis <nesti07@gmail.com>
2018-05-02 19:11:21 -0300

    Nestor - Add redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,15 @@
 import { combineReducers } from 'redux';
 import PostReducer from './reducer_posts';
 
+// This ES6 syntax say get reducer function from redux-form and
+//  put it into the reducer variable, and ten create a new
+// variable formReducer
+import { reducer as formReducer } from 'redux-form';
+
 // This is the App State definition
 const rootReducer = combineReducers({
-  posts: PostReducer
+  posts: PostReducer,
+  form: formReducer
 });
 
 export default rootReducer;
