81d2b71cad03323687d823b742a9ff761ff4d6ca
Nestor Valentinis <nesti07@gmail.com>
2018-05-03 20:59:01 -0300

    Nestor - Ingest Redux-Form, Create Action Creator for CreatePost, Add the form tags

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@
 import axios from 'axios';
 
 export const FETCH_POST = 'FETCH_POST';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = 'key=nvalentinis-redux'
@@ -16,4 +17,13 @@ export function fetchPost() {
     // this is a Promise then redux-promise will unwrapp
     // and leave the response.
   };
+}
+
+export function createPost(props) {
+  const requestPromise = axios.post(`${ROOT_URL}/posts?${API_KEY}`, props);
+
+  return {
+    type: CREATE_POST,
+    payload: requestPromise
+  }
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
 
   render() {
+    const { handleSubmit } = this.props; // const handleSubmit = this.props.handleSubmit
+    const { fields: {title, categories, content} } = this.props; // const title = this.props.fields.title
+
     return (
-      <div>New Posts Form</div>
+      <form onSubmit={ this.props.handleSubmit(this.props.createPost) } >
+        <h3>Create a New Post</h3>
+
+        <div className="form-group" >
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+
+        <div className="form-group" >
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
+        </div>
+
+        <div className="form-group" >
+          <label>Content</label>
+          <textarea className="form-control" {...content} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// on reduxForm we configure the form
+// It will ingect some properties on this.props
+// reduxForm is used in the same way as we used connect to convert
+//  the commponent into a Container.
+// reduxForm receive 3 parameters (ConfigurationObject, mapStateToProps, accionCreators)
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, {createPost} )(PostsNew);
