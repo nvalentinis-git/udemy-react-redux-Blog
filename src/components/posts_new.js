c16bebaed7485d39c56668e7900e33a868cbab33
Nestor Valentinis <nesti07@gmail.com>
2018-05-05 17:11:50 -0300

    Nestor - Configure Redux-Form, Add Validations, Add Cancel Button

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { Link } from 'react-router';
 
 import { createPost } from '../actions/index';
 
@@ -13,26 +14,54 @@ class PostsNew extends Component {
       <form onSubmit={ this.props.handleSubmit(this.props.createPost) } >
         <h3>Create a New Post</h3>
 
-        <div className="form-group" >
+        <div className={ `form-group ${title.touched && title.invalid ? 'has-danger':''}` } >
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
-        <div className="form-group" >
+        <div className={ `form-group ${categories.touched && categories.invalid ? 'has-danger':''}` } >
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
-        <div className="form-group" >
+        <div className={ `form-group ${content.touched && content.invalid ? 'has-danger':''}` } >
           <label>Content</label>
           <textarea className="form-control" {...content} />
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">Save</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
 
       </form>
     );
   }
+}
+
+function validate(values) {
+  const error = {};
+
+  if (!values.title) {
+    error.title = "Enter a username";
+  }
+
+  if (!values.categories) {
+    error.categories = "Enter a Categories";
+  }
+
+  if (!values.content) {
+    error.content = "Enter a Content";
+  }
+
+  return error;
 }
 
 // on reduxForm we configure the form
@@ -42,5 +71,6 @@ class PostsNew extends Component {
 // reduxForm receive 3 parameters (ConfigurationObject, mapStateToProps, accionCreators)
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, {createPost} )(PostsNew);
